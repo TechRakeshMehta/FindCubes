ad030cc211dccc342e37ef30e96cfe0bf600ff3d
arunm <arunm@LAPTOP-J5HN7C5F>
2024-07-19 12:43:07 +0530

    Add project files.

---
+++ FindCubes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Program
+{
+    public static async Task Main()
+    {
+        try
+        {
+            var cancellationTokenSource = new CancellationTokenSource(1000);
+            var token = cancellationTokenSource.Token;
+            int degreeOfParallelism = 4;
+            Task<int[]> task = Task<int[]>.Factory.StartNew((state) =>
+            {
+                var items = Enumerable.Range(1, 1000).ToArray();
+                int degreeOfParallelism = Convert.ToInt32(state);
+                static int map(int x) => x * x * x;
+                var cubes = items.AsParallel().AsOrdered().WithCancellation(token).WithDegreeOfParallelism(Convert.ToInt32(state)).Select(map).ToArray();
+                return cubes;
+            }, cancellationToken: token, state: degreeOfParallelism);
+            int[] result = await task;
+            Console.WriteLine(string.Join(',',result));
+        }
+        catch (OperationCanceledException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        Console.ReadKey();
+    }
+}
+
+
+
